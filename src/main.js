1341058d72b74d7bea206bb5768472b506f18d58
Taavi Laanemaa <taavi.laanemaa@klarna.com>
2022-02-24 18:07:30 +0100

    Added some safety for the script

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
-const cookie = document.getElementById('bigCookie')
-const click = () => { cookie.click(); setTimeout(click, 10) }
-click()
+if (!window.___CLICKER_HX) {
+  console.log("Attaching Cookie Clicker häx!");
+  (() => {
+    const cookie = document.getElementById("bigCookie");
+    const click = () => {
+      cookie.click();
+      setTimeout(click, 10);
+    };
+    click();
+  })();
+}
+
+window.___CLICKER_HX = true;
