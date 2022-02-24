253677d24e2a34bffa043694af08e1e1a318cd19
Taavi Laanemaa <taavi.laanemaa@klarna.com>
2022-02-24 21:18:30 +0100

    Added tag

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -1,6 +1,6 @@
 (() => {
   const script = document.createElement("script");
   script.src =
-    "https://cdn.jsdelivr.net/gh/tlaanemaa/cookie-clicker-hax/src/main.js";
+    "https://cdn.jsdelivr.net/gh/tlaanemaa/cookie-clicker-hax@latest/src/main.js";
   document.head.appendChild(script);
 })();
